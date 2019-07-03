32ec4a6f2665c8bfa795eb3a5b370ecf9b850f71
grounddemon <thegrounddemon@gmail.com>
2019-07-03 15:21:18 -0700

    Updates

---
+++ precache-manifest.372adda660fefd09b2b02754d3032826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ec008d2dc71684ffd18ed304272e58",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "2ba8d80a42893caf5214",
+    "url": "/repository-name/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/repository-name/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2ba8d80a42893caf5214",
+    "url": "/repository-name/static/js/main.55f6b1e3.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/repository-name/static/media/logo.5d5d9eef.svg"
+  }
+]);
